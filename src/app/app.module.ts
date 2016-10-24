3502827fe03e2ff197e64d26f9b6d30e660fd173
Fábio Antunes <fabioantuness@gmail.com>
2016-10-24 16:04:33 +0100

    Removed module error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { YoloComponent } from './yolo/yolo.component';
+
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent,
-    YoloComponent
+    AppComponent
   ],
   bootstrap: [ AppComponent ]
 })
